c07e7b241f20b97eca0b678eb59dfd86111832dc
Alex Nicholls <alex.nicholls@workday.com>
2018-01-21 17:31:30 -0800

    Add file input to select folder for searching

--- src/App.js
+++ src/App.js
@@ -3,6 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sketchFiles: {}
+    }
+  }
+
+  onFileChange(e) {
+    const files = Array.from(e.target.files)
+
+    const sketchFiles = {}
+
+    files.forEach(file => {
+      const extension = file.name.split('.').pop()
+
+      if (extension === 'sketch') {
+        sketchFiles[file.webkitRelativePath] = file
+      }
+    }, this)
+
+    this.setState({ sketchFiles: sketchFiles })
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +37,16 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <input type="file" onChange={this.onFileChange.bind(this)} webkitdirectory="true" directory="true" multiple="true" />
+        <ul>
+          { Object.keys(this.state.sketchFiles).map((path, index) => {
+              const file = this.state.sketchFiles[path]
+
+              return (
+                <li key={index}>{ file.webkitRelativePath }</li>
+              )
+          })}
+        </ul>
       </div>
     );
   }
